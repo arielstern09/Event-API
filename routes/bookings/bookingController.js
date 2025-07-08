9942f3a7c7be7a8081cc67d0898e68529478d701
Ariel Stern <arielstern09@gmail.com>
2025-07-08 11:19:54 -0400

    Add calculations for available tickets and total price

--- routes/bookings/bookingController.js
+++ routes/bookings/bookingController.js
@@ -1,16 +1,41 @@
 const Booking = require("./bookingModel")
+// add Event for ticket availability and pricing data
+const Event = require("../events/eventModel")
+const { getEventById } = require("../events/eventController")
 
 
 // Create Event
+
 const createBooking = async (bookingData) => {
     try {
-        const newBooking = await Booking.create(bookingData)
+        // utilize our existing controller to grab the event by its ID in order for us to calculate the price
+        const event = await getEventById(bookingData.event);
+
+        const newAvailableTickets = event.availableTickets - bookingData.quantity;
+
+        //console.log("NEW TICKETS: " + newAvailableTickets)
+
+        // bookingData.event - the event id
+        await Event.findByIdAndUpdate(bookingData.event, {
+          availableTickets: newAvailableTickets,
+        });
+
+        // copying everything from our booking data into a new object that we are free to modify
+        const calculatedBookingData = {
+            user: bookingData.user,
+            event: bookingData.event,
+            quantity: bookingData.quantity,
+            status: bookingData.status,
+            //totalPrice: our total price calculation
+            totalPrice: bookingData.quantity * event.price
+        }
+
+        const newBooking = await Booking.create(calculatedBookingData)
         return newBooking
     } catch (error) {
         throw error;
     }
 } 
-
 
 
 
