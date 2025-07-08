336148860c3669cc9dec380b04e3d51a4155bbc8
Ariel Stern <arielstern09@gmail.com>
2025-07-08 13:05:35 -0400

    Add filter and sort

--- routes/bookings/bookingController.js
+++ routes/bookings/bookingController.js
@@ -1,46 +1,54 @@
-const Booking = require("./bookingModel")
+const Booking = require("./bookingModel");
 // add Event for ticket availability and pricing data
-const Event = require("../events/eventModel")
-const { getEventById } = require("../events/eventController")
+const Event = require("../events/eventModel");
+const { getEventById } = require("../events/eventController");
+
+const getAllBookings = async () => {
+  try {
+    const bookings = await Booking.find()
+      .populate("user", "username email -_id")
+      .populate("event", "title -_id");
+    return bookings;
+  } catch (error) {
+    throw error;
+  }
+};
 
 
 // Create Event
 
 const createBooking = async (bookingData) => {
-    try {
-        // utilize our existing controller to grab the event by its ID in order for us to calculate the price
-        const event = await getEventById(bookingData.event);
+  try {
+    // utilize our existing controller to grab the event by its ID in order for us to calculate the price
+    const event = await getEventById(bookingData.event);
 
-        const newAvailableTickets = event.availableTickets - bookingData.quantity;
+    const newAvailableTickets = event.availableTickets - bookingData.quantity;
 
-        //console.log("NEW TICKETS: " + newAvailableTickets)
+    //console.log("NEW TICKETS: " + newAvailableTickets)
 
-        // bookingData.event - the event id
-        await Event.findByIdAndUpdate(bookingData.event, {
-          availableTickets: newAvailableTickets,
-        });
+    // bookingData.event - the event id
+    await Event.findByIdAndUpdate(bookingData.event, {
+      availableTickets: newAvailableTickets,
+    });
 
-        // copying everything from our booking data into a new object that we are free to modify
-        const calculatedBookingData = {
-            user: bookingData.user,
-            event: bookingData.event,
-            quantity: bookingData.quantity,
-            status: bookingData.status,
-            //totalPrice: our total price calculation
-            totalPrice: bookingData.quantity * event.price
-        }
+    // copying everything from our booking data into a new object that we are free to modify
+    const calculatedBookingData = {
+      user: bookingData.user,
+      event: bookingData.event,
+      quantity: bookingData.quantity,
+      status: bookingData.status,
+      //totalPrice: our total price calculation
+      totalPrice: bookingData.quantity * event.price,
+    };
 
-        const newBooking = await Booking.create(calculatedBookingData)
-        return newBooking
-    } catch (error) {
-        throw error;
-    }
-} 
-
-
-
+    const newBooking = await Booking.create(calculatedBookingData);
+    return newBooking;
+  } catch (error) {
+    throw error;
+  }
+};
 
 module.exports = {
-    createBooking,
-  
-}
+  createBooking,
+  getAllBookings,
+};
--- routes/events/eventRouter.js
+++ routes/events/eventRouter.js
@@ -1,51 +1,54 @@
 const express = require("express");
-const router = express.Router()
-const {createEvent, getEvents, getEventById} = require("./eventController")
+const router = express.Router();
+
+const { createEvent, getEvents, getEventById } = require("./eventController");
 
 router.get("/", async (req, res) => {
-    try {
-      const events = await getEvents();
-      res.json({
+  try {
+    // events?category=fun&minPrice=5
+    // req.query.category = fun
+    // req.query.minPrice = 5
+    const events = await getEvents(req.query);
+    res.json({
+      message: "success",
+      payload: events,
+    });
+  } catch (error) {
+    res.json({
+      message: "failure",
+      payload: error.message,
+    });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const event = await getEventById(req.params.id)
+    res.status(200).json({
         message: "success",
-        payload: events,
-      });
-    } catch (error) {
-      res.json({
-        message: "failure",
-        payload: error.message,
-      });
-    }
-  });
-  
-  router.get("/:id", async (req, res) => {
-    try {
-      const event = await getEventById(req.params.id)
-      res.status(200).json({
-          message: "success",
-          payload: event
-      })
-    } catch (error) {
-      res.json({
-        message: "failure",
-        payload: error.message,
-      });
-    }
-  });
-  
-  router.post("/", async (req, res) => {
-    try {
-      const newEvent = await createEvent(req.body);
-      res.json({
-        message: "Event has been successfully created",
-        payload: newEvent,
-      });
-    } catch (error) {
-      res.json({
-        message: "failure",
-        payload: error.message,
-      });
-    }
-  });
-  
-  module.exports = router;
-  
+        payload: event
+    })
+  } catch (error) {
+    res.json({
+      message: "failure",
+      payload: error.message,
+    });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newEvent = await createEvent(req.body);
+    res.json({
+      message: "Event has been successfully created",
+      payload: newEvent,
+    });
+  } catch (error) {
+    res.json({
+      message: "failure",
+      payload: error.message,
+    });
+  }
+});
+
+module.exports = router;
